9378455bad7cbf88c720905e3faf0cdd72e7f361
ElnarKast <elnar.kast@tptlive.ee>
2025-03-25 11:04:52 +0200

    Achivements amd golden cookie

--- src/pages/CookieClicker.vue
+++ src/pages/CookieClicker.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, ref } from 'vue';
+import { computed, ref, watch } from 'vue';
 
 const cookies = ref(0);
 const buildings = ref([
@@ -9,41 +9,87 @@ const buildings = ref([
     { name: 'Factory', price: 10_000, cps: 100, count: 0},
 ]);
 
-function buyBuilding(building){
+// Статистика для ачивок
+const achievements = ref({
+  firstClick: false,
+  firstUpgrade: false,
+  goldenCookie: false
+});
+
+// Функция для покупки здания
+function buyBuilding(building) {
     cookies.value -= building.price;
     building.price += Math.ceil(building.price / 100 * 15);
     building.count++;
+    if (!achievements.value.firstUpgrade) {
+        achievements.value.firstUpgrade = true;
+        alert("CONGRATS!NEW ACHIVEMENT");
+    }
 }
+
+// Реактивный расчет CPS
 let cps = computed(() => {
     let cps = 0;
     buildings.value.forEach(building => {
-       cps+=building.cps*building.count;
+        cps += building.cps * building.count;
     });
     return cps;
 });
-setInterval(()=>{
-   cookies.value+=cps.value;
 
-   document.title ='🍪' + +cookies.value.toFixed(1) + ' Cookies!';
-},1000);
+// Обработчик клика по печеньке
+function onCookieClick() {
+    cookies.value++;
+    if (!achievements.value.firstClick) {
+        achievements.value.firstClick = true;
+        alert("CONGRATS!NEW ACHIVEMENT");
+    }
+}
 
+// Секундный интервал, для накопления печенек
+setInterval(() => {
+    cookies.value += cps.value;
+
+    // Появление золотой печеньки с шансом 1/1000
+    if (!achievements.value.goldenCookie && Math.random() < 0.001) {
+        achievements.value.goldenCookie = true;
+        alert("Поздравляем! Золотая печенька появилась! Получите бонус!");
+        cookies.value += 1000;  // например, 1000 дополнительных печенек
+    }
+
+    document.title = '🍪' + cookies.value.toFixed(1) + ' Cookies!';
+}, 1000);
 </script>
+
 <template>
     <div class="columns">
+
         <div class="column is-4 has-background-primary has-text-centered">
             <h1 class="is-size-1">{{ +cookies.toFixed(1) }} cookies</h1>
             <h3 class="is-size-3">{{ +cps.toFixed(1) }} cps</h3>
-            <figure class="image is-square" @click="cookies++">
+
+            <figure class="image is-square" @click="onCookieClick">
                 <img src="https://sweetlorens.com/cdn/shop/products/Copy-of-Chocolate-Chunk-Full-Cookie-transparent-background.png?v=1687811511" />
             </figure>
         </div>
-        <div class="column is-6 has-background-link">
-            asdas
-        </div>
+
+
         <div class="column is-2 has-background-warning">
-            <button v-for="building in buildings" :disabled="cookies<building.price" @click="buyBuilding(building)" class="button is-primary is-large is-fullwidth">
+            <button v-for="building in buildings" 
+                    :disabled="cookies<building.price" 
+                    @click="buyBuilding(building)" 
+                    class="button is-primary is-large is-fullwidth">
                 {{ building.name }} 🍪{{ building.price }} #{{ building.count }}
             </button>
         </div>
+    </div>
+
+
+    <div v-if="achievements.firstClick || achievements.firstUpgrade || achievements.goldenCookie" class="notification is-info">
+        <h4 class="title">Your achievements:</h4>
+        <ul>
+            <li v-if="achievements.firstClick">first click</li>
+            <li v-if="achievements.firstUpgrade">first upgrade</li>
+            <li v-if="achievements.goldenCookie"> ITS GOOOLDEEEEN</li>
+        </ul>
     </div>
 </template>
