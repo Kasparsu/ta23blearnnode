b0a85445ff2b801a7c76ed47ffbb629a8276a16a
Kaspar Bergert <kasparbergert6@gmail.com>
2025-03-29 21:35:40 +0200

    made the cookie move when clicked

--- src/pages/CookieClicker.vue
+++ src/pages/CookieClicker.vue
@@ -3,13 +3,13 @@ import { computed, ref } from 'vue';
 
 const cookies = ref(0);
 const buildings = ref([
-    { name: 'Cursor', price: 15, cps: 0.1, count: 0},
-    { name: 'Grandma', price: 100, cps: 1, count: 0},
-    { name: 'Farm', price: 1000, cps: 10, count: 0},
-    { name: 'Factory', price: 10_000, cps: 100, count: 0},
+    { name: 'Cursor', price: 15, cps: 0.1, count: 0 },
+    { name: 'Grandma', price: 100, cps: 1, count: 0 },
+    { name: 'Farm', price: 1000, cps: 10, count: 0 },
+    { name: 'Factory', price: 10, cps: 100, count: 0 },
 ]);
 
-function buyBuilding(building){
+function buyBuilding(building) {
     cookies.value -= building.price;
     building.price += Math.ceil(building.price / 100 * 15);
     building.count++;
@@ -17,33 +17,50 @@ function buyBuilding(building){
 let cps = computed(() => {
     let cps = 0;
     buildings.value.forEach(building => {
-       cps+=building.cps*building.count;
+        cps += building.cps * building.count;
     });
     return cps;
 });
-setInterval(()=>{
-   cookies.value+=cps.value;
+setInterval(() => {
+    cookies.value += cps.value;
 
-   document.title ='🍪' + +cookies.value.toFixed(1) + ' Cookies!';
-},1000);
+    document.title = '🍪' + +cookies.value.toFixed(1) + ' Cookies!';
+}, 1000);
 
 </script>
 <template>
     <div class="columns">
-        <div class="column is-4 has-background-primary has-text-centered">
-            <h1 class="is-size-1">{{ +cookies.toFixed(1) }} cookies</h1>
-            <h3 class="is-size-3">{{ +cps.toFixed(1) }} cps</h3>
-            <figure class="image is-square" @click="cookies++">
-                <img src="https://sweetlorens.com/cdn/shop/products/Copy-of-Chocolate-Chunk-Full-Cookie-transparent-background.png?v=1687811511" />
+        <div class="column is-4 has-background-white has-text-centered">
+            <h1 class="is-size-1 has-text-black">{{ +cookies.toFixed(1) }} cookies</h1>
+            <h3 class="is-size-3 has-text-black">{{ +cps.toFixed(1) }} cps</h3>
+            <figure class="image is-square cookie-move" @click="cookies++">
+                <img
+                    src="https://sweetlorens.com/cdn/shop/products/Copy-of-Chocolate-Chunk-Full-Cookie-transparent-background.png?v=1687811511" />
             </figure>
         </div>
-        <div class="column is-6 has-background-link">
+
+        <div class="column is-6 has-background-white has-text-black">
             asdas
         </div>
-        <div class="column is-2 has-background-warning">
-            <button v-for="building in buildings" :disabled="cookies<building.price" @click="buyBuilding(building)" class="button is-primary is-large is-fullwidth">
+
+
+
+        <div class="column is-2 has-background-white">
+            <button v-for="building in buildings" :disabled="cookies < building.price" @click="buyBuilding(building)"
+                class="button is-primary is-large is-fullwidth">
                 {{ building.name }} 🍪{{ building.price }} #{{ building.count }}
             </button>
         </div>
     </div>
 </template>
+
+<style scoped>
+.cookie-move {
+  transition: transform 0.1s ease;
+}
+.cookie-move:active {
+  transform: scale(0.9) rotate(5deg);
+}
+
+
+</style>
